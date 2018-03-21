5622ef3f45ddbc4cd8794a2e1feb459c954c0230
renmingming <846992190@qq.com>
2018-03-21 23:33:23 +0800

    next

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -9,17 +9,24 @@
       <mu-tab value="job" title="招聘" />
     </mu-tabs>
     <div class="tab-content">
+      <mu-refresh-control :refreshing="refreshing" :trigger="scroller" @refresh="refresh" />
       <mu-list>
         <mu-list-item v-for="item in tabData" :key="item.id">
           <mu-avatar slot="left" :src="item.author.avatar_url" :size="40"></mu-avatar>
           <div class="item-list-info">
             <div class="title">
-              <span v-if="item.top" class="top">置顶</span>
-              <span v-else-if="item.good" class="good">精华</span>
-              <span class="category">{{item.tab | tab}}</span>
+              <div v-if="activeTab==='all' || activeTab==='good'" class="title-tip">
+                <span v-if="item.top" class="top tip">置顶</span>
+                <span v-else-if="item.good" class="good tip">精华</span>
+                <span v-else class="category tip">{{item.tab | tab}}</span>
+              </div>
               {{item.title}}
             </div>
-            <div class="info"></div>
+            <div class="info">
+              <span class="author-name">{{item.author.loginname}}</span>
+              <span class="count reply">回复：{{item.reply_count}}</span>
+              <span class="count visit">访问量：{{item.visit_count}}</span>
+            </div>
           </div>
         </mu-list-item>
       </mu-list>
@@ -38,6 +45,7 @@ export default {
       activeTab: 'all',
       url: 'https://www.vue-js.com/api/v1/topics',
       loading: false,
+      refreshing: false,
       scroller: null,
       tabData: []
     }
@@ -49,10 +57,12 @@ export default {
     this.scroller = this.$el
   },
   methods: {
+    // tab铁环
     handleTabChange (val) {
       this.activeTab = val
       this.getData(val)
     },
+    // 获取数据
     getData (val) {
       let that = this
       axios.get(this.url, {
@@ -64,6 +74,7 @@ export default {
         console.log(error)
       })
     },
+    // 上划加载
     loadMore () {
       this.loading = true
       let that = this
@@ -71,9 +82,24 @@ export default {
       axios.get(this.url, {
         params: {'tab': that.activeTab, 'page': that.page}
       }).then(function (response) {
-        that.tabData = that.tabData.concat(response.data.data)
+        that.tabData = Object.assign(that.tabData, response.data.data)
         console.log(response.data.data)
         that.loading = false
+      }).catch(function (error) {
+        console.log(error)
+      })
+    },
+    // 下拉刷新
+    refresh () {
+      this.refreshing = true
+      let that = this
+      axios.get(this.url, {
+        params: {'tab': that.activeTab, 'page': 1}
+      }).then(function (response) {
+        // that.tabData = that.tabData.concat(response.data.data) 后面得覆盖前面得
+        that.tabData = Object.assign(that.tabData, response.data.data)
+        console.log(response.data.data)
+        that.refreshing = false
       }).catch(function (error) {
         console.log(error)
       })
@@ -93,6 +119,41 @@ export default {
   }
   .tab-content{
     padding-top:48px;
+    .item-list-info{
+      .title{
+        .title-tip{
+          display:inline-block;
+        }
+        .tip{
+          display:inline-block;
+          padding:1px 5px;
+          border-radius:3px;
+          color:#fff;
+          font-size:12px;
+        }
+        .top{
+          background:#369219;
+        }
+        .good{
+          background:#7e57c2;
+        }
+        .category{
+          color:#666;
+          background:#e2e2e2;
+        }
+      }
+      .info{
+        padding-top:5px;
+        .author-name{
+          padding-right:20px;
+        }
+        .count{
+          padding-right:10px;
+          font-size:12px;
+          color:#666;
+        }
+      }
+    }
   }
 }
 </style>
